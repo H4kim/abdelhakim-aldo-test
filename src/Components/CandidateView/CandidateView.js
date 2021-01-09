949a67db7e0fd1aec92231bf4d8539d2ae06b3b0
Hakim <ozil.hakim@gmail.com>
2021-01-08 21:28:28 -0500

    create candidate component

---
+++ src/Components/CandidateView/CandidateView.js
@@ -0,0 +1,12 @@
+import classes from './CandidateView.module.css'
+import React from 'react'
+
+const CandidateView = () => {
+    return (
+        <div className={classes.container}>
+            Test here yo 
+        </div>
+    )
+}
+
+export default CandidateView
--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -2,6 +2,7 @@ import classes from './Home.module.css'
 import React from 'react'
 import PrimaryTitle from '../../Components/PrimaryTitle/PrimaryTitle'
 import PrimaryButton from '../../Components/PrimaryButton/PrimaryButton'
+import CandidateView from '../../Components/CandidateView/CandidateView'
 
 const Home = () => {
     const switchViewHandler = () => {
@@ -11,6 +12,9 @@ const Home = () => {
         <div className={classes.container}>
             <PrimaryTitle text={'Latest tweets'} />
             <PrimaryButton text={'Switch Candidate'} clicked={switchViewHandler} />
+            <div className={classes.candidateContainer}>
+                <CandidateView />
+            </div>
         </div>
     )
 }
